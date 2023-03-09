a5b7054b9b1fce344c1c2f434ed044d58936f2b8
DouglasDans <douglas.dans@gmail.com>
2023-03-09 10:36:02 -0300

    Aula04

---
+++ Aula04/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        Lista lista = new Lista();
+
+        lista.add("Douglas");
+        lista.add("Jack");
+        lista.add("Predo");
+        lista.add("vito");
+        lista.add("gutasvo");
+
+        lista.imprimir();
+    }
+}
---
+++ Aula04/src/No.java
@@ -0,0 +1,9 @@
+public class No {
+    Object info;
+    No proximo;
+
+    No(Object info){
+        this.info = info;
+        proximo = null;
+    }
+}
